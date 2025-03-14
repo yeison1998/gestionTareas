8c09fba0d87adf8584e63592c92480a9003f8ab4
yeison6 <yeison@innclod.com>
2025-03-14 08:38:53 -0500

    Agregar funcionalidades  de editar, eliminar y pasar a completadas

--- src/app/components/task-management/task-management.component.ts
+++ src/app/components/task-management/task-management.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TaskManagement } from '../interfaces/task-management';
 import { AbstractControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-task-management',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-management.component.html',
   styleUrl: './task-management.component.css'
 })
@@ -15,7 +16,7 @@ export class TaskManagementComponent implements OnInit {
 
   formTask!: FormGroup;
   fb = inject(FormBuilder);
-
+  badge = signal(0);
 
   ngOnInit(): void {
     this.setForm();
@@ -30,11 +31,40 @@ export class TaskManagementComponent implements OnInit {
     });
   }
 
+  changeComplete(complete: TaskManagement) {
+    this.tasks.update(tasks => tasks.map(task => {
+      if (task.id === complete.id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    }));
+  }
+
+  changeImportant(importante: TaskManagement) {
+    this.tasks.update(tasks => tasks.map(task => {
+      if (task.id === importante.id) {
+        task.isImportant = !task.isImportant;
+      }
+      return task;
+    }));
+  }
+
   addTask() {
     this.id.setValue(this.id.value + 1);
-    console.log(this.formTask.value);
-
     this.tasks.update(tasks => [...tasks, this.formTask.value]);
+    console.log(this.tasks().length);
+     
+    this.badge.set(this.tasks().length);
+  }
+
+  updateTask(taskUpdate: TaskManagement) {
+    this.tasks.update(tasks => tasks.map(task => {
+      if (task.id === taskUpdate.id) {
+        task.isImportant = !task.isImportant;
+        return {...task, ...taskUpdate}
+      }
+      return task;
+    }));
   }
 
   get id(): AbstractControl { return this.formTask.get('id')! }
